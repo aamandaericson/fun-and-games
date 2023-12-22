66dd1cf49033c727c37fb0c998a4048b53c625df
aamandaericson <>
2023-12-22 11:25:56 +0000

    Remove secret key

--- fun_and_games/settings.py
+++ fun_and_games/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-()+xvgm3z*rm=@2tc0+%z+pn8=28h77pz(k&j19#wo6ybp^v-l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-aamandaericson-fun-and-g-09tx5yia8p.us2.codeanyapp.com', 'fun-and-games-app', 'localhost']
